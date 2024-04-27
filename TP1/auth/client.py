5fea1f0a485db7a94e5f2440ac886da6db787031
Samuel Viana <samuelsnv2@gmail.com>
2024-04-27 14:02:37 +0000

    client auth implemented

--- TP1/auth/client.py
+++ TP1/auth/client.py
@@ -1,8 +1,37 @@
+from auth.messager import *
 import socket
 import struct
 import sys
 
 def auth(host, port, command):
+    '''
+    Check if at least 3 args are provided - host, port and command
+    extract host and port from the command
+    '''
     host = str(host).strip()
     port = int(port)
-    program = command[0]
+    type = command[0]
+    try:
+        messager_o = messager(type=type)
+    except Exception as e:
+        return e
+    sock = socket.socket(determineIpType(host), socket.SOCK_STREAM) # create a socket
+    sock.settimout(404)
+    try:
+        sock.connect((host, port))
+        packet = messager_o.request(command[1:])
+        sock.send(packet, (host, port)) # send the request
+        resp, addr = sock.recvfrom(4000) # receive the response
+        try:
+            messager_o.checkErrorM(resp)
+        except RequestError as e:
+            raise e
+        r = messager_o.response(resp)
+        return r
+    except socket.timeout:
+        print('Timeout: No response received from the server. Retrying...')
+    except Exception as e:
+        print('Connection error: ', e)
+        raise e
+    # close the socket
+    sock.close()
